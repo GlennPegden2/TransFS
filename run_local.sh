5db9c3393ba872ff1b60611dec5586f715339305
Glenn Pegden <glenn@pegden.com>
2025-06-03 00:22:29 +0100

    A different approach to logging

--- run_local.sh
+++ run_local.sh
@@ -24,7 +24,7 @@ cp ./smb.conf /etc/samba/smb.conf
 
 cd app
 service smbd start &&
-python3 transfs.py &
+python3 transfs.py 2>&1 | tee /tmp/transfs.log &
 
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload &&
 tail -f /dev/null
